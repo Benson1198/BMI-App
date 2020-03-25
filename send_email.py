034d48e8373795e0fc066333edab1b17398a12f4
Benson <bensonpx1198@gmail.com>
2020-03-25 23:49:55 +0530

    final

--- send_email.py
+++ send_email.py
@@ -1,7 +1,15 @@
 import smtplib
+
+
 email_list = []
+
+
 email_id = input('Enter your email id: ')
 password_log = input("Enter your password: ")
+subject = input('Enter the subject of mail: ')
+email_body = input('Enter the email body: ')
+
+
 n = int(input('Enter no. of recipients: '))
 for x in range(n):
     email_str = input('Recpient email %d : '%(x+1))
@@ -11,4 +19,5 @@ for x in range(n):
 smtpObj = smtplib.SMTP_SSL('smtp.gmail.com',465)
 smtpObj.ehlo()
 smtpObj.login(email_id,password_log)
-smtpObj.sendmail(email_id ,email_list, 'Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Ben')
+message_body = 'Subject:' +  subject + '\n' + email_body
+smtpObj.sendmail(email_id ,email_list, message_body)
