d990693eb2be2860aaae2dc8216b7358f4d2d9df
Benson <bensonpx1198@gmail.com>
2020-04-03 23:49:43 +0530

    logic

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -7,6 +7,7 @@ import 'constants.dart';
 import 'results_page.dart';
 import 'bottom_button.dart';
 import 'round_icon_button.dart';
+import 'calculator_brain.dart';
 
 enum Gender { male, female }
 
@@ -201,9 +202,15 @@ class _InputPageState extends State<InputPage> {
             BottomButton(
                 buttonTitle: "CALACULATE",
                 onTap: () {
+                  CalculatorBrain calc =
+                      CalculatorBrain(height: height, weight: weight);
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) {
-                      return ResultsPage();
+                      return ResultsPage(
+                        bmiResult: calc.calculateBMI(),
+                        resultText: calc.getResult(),
+                        interpretation: calc.getInterpretation(),
+                      );
                     },
                   ));
                 }),
